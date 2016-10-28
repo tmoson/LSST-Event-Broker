f1939ad1cd9551e376b17697bb8a298f879bb138
Stephen DiCerce <sdicerce@oswego.edu>
2016-10-28 13:34:16 -0400

    Made Function Generator and Model Transient
    Model transient is still without many of the descriptive variables we will need, those will be in shortly

---
+++ Functiongenerator.py
@@ -0,0 +1,12 @@
+import math
+'''The purpose of this class is to generate
+simple functions with the intent of finding the probability
+that an observation belongs to one of the two functions'''
+
+#defines a function y = 4
+def get_function_1():
+    return 4
+
+#defines a function y = sin(x)
+def get_function_2(x):
+    return math.sin(x)
